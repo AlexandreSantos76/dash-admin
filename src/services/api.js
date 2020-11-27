10e698b7195c275208747f7fd7cfb56dc4dbf858
Alexandre O Santos <alexandreosantos.ti@gmail.com>
2020-11-27 08:07:39 -0300

    Variaveis de Ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const getToken = () => localStorage.getItem('@MonetizDB:token');
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: process.env.REACT_APP_API_PATH
 });
 
 
