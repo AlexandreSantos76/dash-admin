531cdedb72afe350ee67b25c99bd343df60ec1de
Alexandre O Santos <alexandreosantos.ti@gmail.com>
2021-03-12 12:34:11 -0300

    Extratos tabela transações

--- src/hooks/billings.js
+++ src/hooks/billings.js
@@ -1,7 +1,7 @@
 import Api from "../services/api"
 
 export const statement = (props) => {
-    return Api.get(`gateway/statement?init=${props.init}&end=${props.end}`)
+    return Api.get(`gateway/statement?transaction_date_end=${props.init}&transaction_date_end=${props.end}&liquidation_date_init=${props.init}&liquidation_date_end=${props.end}`)
         .then((result) => {
             return result.data
         }).catch((err) => {
