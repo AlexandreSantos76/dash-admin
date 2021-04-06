126c46f5bf9d38ec93b3a8f515ac3d0066d94807
Alexandre O Santos <alexandreosantos.ti@gmail.com>
2021-04-06 17:58:08 -0300

    Statetment

--- src/hooks/CodeBlock.js
+++ src/hooks/CodeBlock.js
@@ -4,7 +4,7 @@ import { CodeBlock, dracula } from "react-code-blocks";
 export default function MyCoolCodeBlock({ code }) {
   return (
     <CodeBlock
-      text={JSON.stringify(JSON.parse(code),null,2)}      
+      text={code !== null ? JSON.stringify(JSON.parse(code),null,2):""}      
       theme={dracula}
       language="json"
       wrapLines
