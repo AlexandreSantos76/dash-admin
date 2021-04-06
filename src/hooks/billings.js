27d269250d487c1ac1fabcca794eb622489ccf89
Alexandre O Santos <alexandreosantos.ti@gmail.com>
2021-04-06 15:27:48 -0300

    Statetment

--- src/hooks/billings.js
+++ src/hooks/billings.js
@@ -1,13 +1,14 @@
 import Api from "../services/api"
 
 export const statement = (props) => {
-    return Api.get(`gateway/statement?transaction_date_end=${props.init}&transaction_date_end=${props.end}`)
+    return Api.get(`gateway/statement?transaction_date_end=${props.init}&transaction_date_end=${props.end}&liquidation_date_init=${props.init}&liquidation_date_end=${props.end}`)
         .then((result) => {
             return result.data
         }).catch((err) => {
             console.log(err);
         });
 }
+
 export const refused = (props) => {
     return Api.get(`gateway/statement-refuseds?transaction_date_end=${props.init}&transaction_date_end=${props.end}`)
         .then((result) => {
