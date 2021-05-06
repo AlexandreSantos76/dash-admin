02330d54c70bb66f1b5e3691cf42b4f3921c20fd
Alexandre O Santos <alexandreosantos.ti@gmail.com>
2021-05-06 11:01:30 -0300

    statement

--- src/components/Statements/TableLiquidation.js
+++ src/components/Statements/TableLiquidation.js
@@ -4,10 +4,16 @@ import MUIDataTable, { ExpandButton } from "mui-datatables";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
-import Table from "reactstrap/lib/Table";
 import moment from "moment";
 import "moment/locale/pt-br";
-import { Modal, ModalHeader, ModalFooter, ModalBody, Button } from "reactstrap";
+import {
+  Modal,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Button,
+  Table,
+} from "reactstrap";
 import { formatCNPJ } from "utils/FormateUtils";
 import { formatCurrency } from "utils/FormateUtils";
 import api from "services/api";
@@ -15,23 +21,96 @@ import Row from "reactstrap/lib/Row";
 import Col from "reactstrap/lib/Col";
 import { formatCPF } from "utils/FormateUtils";
 import { liquidation } from "../../hooks/billings";
+import DatetimeRangePicker from "react-datetime-range-picker";
 const TableLiquidations = () => {
   const [transactions, setTransactions] = useState([]);
   const [orders, setOrders] = useState([]);
   const [comissions, setComissions] = useState([]);
   const [init, setInit] = useState(moment().clone().startOf("month").format());
   const [end, setEnd] = useState(moment().format());
+  const [responses, setResponses] = useState([]);
   useEffect(() => {
     const getSumary = async () => {
       let data = await liquidation({ init: init, end: end });
+      let ord = data.orders;
+      let arrayResponse = [];
+      ord.forEach((order) => {
+        let tr = order.transations;
+        if (
+          tr !== null &&
+          typeof tr === "object" &&
+          tr.hasOwnProperty("response")
+        ) {
+          let res = JSON.parse(order?.transations?.response);
+
+          if (res !== null && res.hasOwnProperty("payment_id")) {
+            arrayResponse.push(res);
+          }
+        }
+      });
+      setResponses(arrayResponse);
       setOrders(data.orders);
       setTransactions(data.transactions.list_transactions);
       setComissions(data.transactions.commission);
     };
     getSumary();
   }, [init, end]);
+  const setRange = (e) => {
+    setInit(moment(e.start).format());
+    setEnd(moment(e.end).format());
+  };
 
-  return <></>;
+  const getOrder = async (payment) => {
+    let ordId = await responses.find((o) => o.payment_id === payment);
+    console.log(ordId.order_id);
+    let order = await orders.find((o) => o.id === parseInt(ordId.order_id));
+    return order;
+  };
+  //console.log(orders);
+  console.log(comissions);
+  if (orders.length) {
+    getOrder("6065b3a0e17e440011e2581b");
+  }
+  return (
+    <>
+      <Row>
+        <Col xs="6">
+          <DatetimeRangePicker
+            className="d-flex"
+            inline={true}
+            startDate={init}
+            endDate={end}
+            locale="pt-br"
+            pickerClassName="col-6"
+            onChange={setRange}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Table>
+          <thead>
+            <tr>
+              <th>Pedido</th>
+              <th>Produto</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+              {comissions.map((data,key)=>{
+                  //let order = getOrder(data)
+                  return(
+                      <tr key={key}>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                  )
+              })}
+          </tbody>
+        </Table>
+      </Row>
+    </>
+  );
 };
 
 export default TableLiquidations;
