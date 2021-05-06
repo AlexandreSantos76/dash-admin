8ee52438f5630da7223bec6b6c6cda39747bd449
Alexandre O Santos <alexandreosantos.ti@gmail.com>
2021-05-06 18:52:05 -0300

    statement

--- src/components/Statements/TableLiquidation.js
+++ src/components/Statements/TableLiquidation.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "fontsource-roboto";
 import MUIDataTable, { ExpandButton } from "mui-datatables";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
@@ -24,33 +24,27 @@ import { liquidation } from "../../hooks/billings";
 import DatetimeRangePicker from "react-datetime-range-picker";
 const TableLiquidations = () => {
   const [transactions, setTransactions] = useState([]);
+  const [summary, setSummary] = useState([]);
   const [orders, setOrders] = useState([]);
   const [comissions, setComissions] = useState([]);
   const [init, setInit] = useState(moment().clone().startOf("month").format());
   const [end, setEnd] = useState(moment().format());
-  const [responses, setResponses] = useState([]);
   useEffect(() => {
     const getSumary = async () => {
       let data = await liquidation({ init: init, end: end });
-      let ord = data.orders;
-      let arrayResponse = [];
-      ord.forEach((order) => {
-        let tr = order.transations;
-        if (
-          tr !== null &&
-          typeof tr === "object" &&
-          tr.hasOwnProperty("response")
-        ) {
-          let res = JSON.parse(order?.transations?.response);
-
-          if (res !== null && res.hasOwnProperty("payment_id")) {
-            arrayResponse.push(res);
-          }
-        }
+      let dataTransactions = data.transactions.list_transactions;
+      let details = [];
+      let dataSummary = [];
+      dataTransactions.forEach((tr) => {
+        dataSummary.push(tr.summary);
+        tr.details.forEach((dt) => {
+          details.push(dt);
+        });
       });
-      setResponses(arrayResponse);
+
       setOrders(data.orders);
-      setTransactions(data.transactions.list_transactions);
+      setTransactions(details);
+      setSummary(dataSummary);
       setComissions(data.transactions.commission);
     };
     getSumary();
@@ -60,58 +54,309 @@ const TableLiquidations = () => {
     setEnd(moment(e.end).format());
   };
 
-  const getOrder = async (payment) => {
-    let ordId = await responses.find((o) => o.payment_id === payment);
-    let order = await orders.find((o) => o.id === parseInt(ordId.order_id));
+  const getSummary = (tid) => {
+    let ord = summary.find((o) => o.marketplace_transaction_id === tid);
+    return ord;
+  };
+
+  const getOrder = (id) => {
+    let order = orders.find((o) => o.id === parseInt(id));
     return order;
   };
-  //console.log(orders);
-  console.log(comissions);
-  if (orders.length) {
-    getOrder("6065b3a0e17e440011e2581b");
-  }
+
+  const getDetail = (tid) => {
+    let detail = transactions.find((o) => o.marketplace_transaction_id === tid);
+
+    return detail;
+  };
+
+  const getTableRows = () => {
+    let rows = [];
+    comissions.forEach((data, key) => {
+      let detail = getDetail(data.marketplace_transaction_id);
+      let sum = getSummary(data.marketplace_transaction_id);
+      //let order = getOrder(sum.order_id);
+      let compare = moment(end);
+      let compareNow = moment(data.subseller_rate_confirm_date);
+      if (compareNow < compare) {
+        let tableRow = {
+          order: sum?.order_id,
+          item: data.item_id,
+          data: data.transaction_date,
+          release: detail?.release_status,
+          previsao: data.payment_date,
+          confirm: data.subseller_rate_confirm_date,
+          total: data.installment_amount,
+          mdr: data.mdr_rate_ammount,
+          comissao: data.subseller_rate_amount,
+        };
+
+        rows.push(tableRow);
+      }
+    });
+    return rows;
+  };
+  const dataCols = [
+    {
+      label: "Pedido",
+      name: "order",
+      options: {
+        filter: true,
+      },
+    },
+    {
+      label: "Produto",
+      name: "item",
+      options: {
+        filter: true,
+      },
+    },
+    {
+      label: "Data",
+      name: "data",
+      options: {
+        filter: true,
+        customBodyRender: (value) => {
+          return <>{moment(value).format("DD/MM/YY")}</>;
+        },
+      },
+    },
+    {
+      label: "Liberado",
+      name: "release",
+      options: {
+        filter: false,
+      },
+    },
+
+    {
+      label: "Previsão",
+      name: "previsao",
+      options: {
+        filter: true,
+        customBodyRender: (value) => {
+          return <>{moment(value).format("DD/MM/YY")}</>;
+        },
+      },
+    },
+    {
+      label: "Pagamento",
+      name: "confirm",
+      options: {
+        filter: true,
+        customBodyRender: (value) => {
+          return <>{value && moment(value).format("DD/MM/YY")}</>;
+        },
+      },
+    },
+    {
+      label: "Valor",
+      name: "total",
+      options: {
+        filter: false,
+        customBodyRender: (value) => <>{formatCurrency(value / 100)}</>,
+      },
+    },
+    {
+      label: "MDR",
+      name: "mdr",
+      options: {
+        filter: false,
+        customBodyRender: (value) => <>{formatCurrency(value / 100)}</>,
+      },
+    },
+    {
+      label: "Comissão",
+      name: "comissao",
+      options: {
+        filter: false,
+        customBodyRender: (value) => <>{formatCurrency(value / 100)}</>,
+      },
+    },
+  ];
+  const options = {
+    filter: true,
+    filterType: "dropdown",
+    pagination: true,
+    rowsPerPage: 50,
+    download: true,
+    print: true,
+    sort: true,
+    search: true,
+    viewColumns: false,
+    selectableRows: "none",
+    textLabels: {
+      body: {
+        noMatch: "Nenhum registro encontrado",
+      },
+    },
+    sortOrder: {
+      name: "order",
+      direction: "desc",
+    },
+  };
+  const getMuiTheme = () =>
+    createMuiTheme({
+      shadows: Array(25).fill("none"),
+      overrides: {
+        MUIDataTableSelectCell: {
+          expandDisabled: {
+            // Soft hide the button.
+            visibility: "hidden",
+          },
+          root: {
+            backgroundColor: "#e9e9e9",
+            padding: "5px",
+          },
+          body: {
+            border: "1px solid rgba(224, 224, 224, 1)",
+          },
+        },
+        MUIDataTableBodyRow: {
+          root: {
+            "&:nth-child(odd)": {
+              backgroundColor: "#f6f8fa",
+            },
+          },
+        },
+        MUIDataTableBodyCell: {
+          root: {
+            padding: "5px",
+          },
+        },
+        MUIDataTableHeadRow: {
+          root: {
+            backgroundColor: "#FF0000",
+          },
+        },
+        MuiTableSortLabel: {
+          root: {
+            color: "#ffffff",
+            "&:hover": {
+              color: "#ffffff",
+
+              "&& $icon": {
+                opacity: 1,
+                color: "#ffffff",
+              },
+            },
+            "&$active": {
+              color: "#ffffff",
+
+              // && instead of & is a workaround for https://github.com/cssinjs/jss/issues/1045
+              "&& $icon": {
+                opacity: 1,
+                color: "#ffffff",
+              },
+            },
+          },
+        },
+        MUIDataTableHeadCell: {
+          sortActive: {
+            color: "#ffffff",
+          },
+          sortAction: {
+            color: "#ffffff",
+          },
+          fixedHeader: {
+            backgroundColor: "#FF0000",
+            padding: "5px",
+          },
+          data: {
+            color: "#ffffff",
+            fontWeight: "bold",
+          },
+        },
+      },
+    });
+  const memoExtract = () => {
+    let jr = 0;
+    let jp = 0;
+    let cr = 0;
+    let cp = 0;
+    comissions.forEach((data, key) => {
+      let r = data.subseller_rate_confirm_date ? true : false;
+      let compare = moment(end);
+      let compareNow = moment(data.subseller_rate_confirm_date);
+      if (compareNow < compare) {
+        if (data.item_id.match(/tax/)) {
+          if (r) {
+            jr = jr + data.installment_amount - data.mdr_rate_ammount;
+          } else {
+            jp = jp + data.installment_amount - data.mdr_rate_ammount;
+          }
+        } else {
+          if (r) {
+            cr = cr + data.subseller_rate_amount;
+          } else {
+            cp = cp + data.subseller_rate_amount;
+          }
+        }
+      }
+    });
+
+    return (
+      <Table>
+        <tbody>
+          <tr>
+            <td className="text-right">
+              <strong>Juros Recebidos:</strong>
+            </td>
+            <td>{formatCurrency(jr / 100)}</td>
+            <td className="text-right">
+              <strong>Comissões Recebidas:</strong>
+            </td>
+            <td>{formatCurrency(cr / 100)}</td>
+          </tr>
+          <tr>
+            <td className="text-right">
+              <strong>Juros á Receber:</strong>
+            </td>
+            <td>{formatCurrency(jp / 100)}</td>
+            <td className="text-right">
+              <strong>Comissões á Receber:</strong>
+            </td>
+            <td>{formatCurrency(cp / 100)}</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
+  };
   return (
     <>
-      <Row>
-        <Col xs="6">
-          <DatetimeRangePicker
-            className="d-flex"
-            inline={true}
-            startDate={init}
-            endDate={end}
-            locale="pt-br"
-            pickerClassName="col-6"
-            onChange={setRange}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Table>
-          <thead>
-            <tr>
-              <th>Pedido</th>
-              <th>Produto</th>
-              <th>Valor Total</th>
-              <th>MDR</th>
-              <th>Comissão</th>
-            </tr>
-          </thead>
-          <tbody>
-              {comissions.map((data,key)=>{
-                  //let order = getOrder(data.payment_id)
-                  return(
-                      <tr key={key}>
-                          <td>{data.payment_id}</td>
-                          <td>{data.item_id}</td>
-                          <td>{data.installment_amount}</td>
-                          <td>{data.mdr_rate_ammount}</td>
-                          <td>{data.subseller_rate_amount}</td>
-                      </tr>
-                  )
-              })}
-          </tbody>
-        </Table>
-      </Row>
+      {transactions && (
+        <div>
+          <Row className="mb-5">
+            <Col>{memoExtract()}</Col>
+          </Row>
+          <Row>
+            <Col xs="6"></Col>
+            <Col xs="6">
+              <DatetimeRangePicker
+                className="d-flex h3"
+                inline={true}
+                startDate={init}
+                endDate={end}
+                locale="pt-br"
+                pickerClassName="col-6"
+                onChange={setRange}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <MuiThemeProvider theme={getMuiTheme()}>
+                <MUIDataTable
+                  title={"Comissões"}
+                  data={getTableRows()}
+                  columns={dataCols}
+                  options={options}
+                />
+              </MuiThemeProvider>
+            </Col>
+          </Row>
+        </div>
+      )}
     </>
   );
 };
--- src/hooks/billings.js
+++ src/hooks/billings.js
@@ -19,7 +19,7 @@ export const refused = (props) => {
 }
 
 export const liquidation = (props) => {
-    return Api.get(`gateway/statement?transaction_date_init=${props.init}&transaction_date_end=${props.end}&liquidation_date_init=${props.init}&liquidation_date_end=${props.end}`)
+    return Api.get(`gateway/statement?transaction_date_init=${props.init}&transaction_date_end=${props.end}&schedule_date_init=${props.init}&schedule_date_date_end=${props.end}`)
         .then((result) => {
             return result.data
         }).catch((err) => {
